6161c51a35dcc4a24e5ac38072a8756cf265d9bf
Alimbetov <alimbetovpost@gmail.com>
2023-05-23 19:39:39 +0600

    lesson6 fix002

--- lib/widgets/action_button.dart
+++ lib/widgets/action_button.dart
@@ -1,31 +1,33 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:superheroes/resources/superheroes_Colors.dart';
 
 class ActionButton extends StatelessWidget {
-
   final String text;
   final VoidCallback onTap;
 
-  const ActionButton({Key? key, required this.text, required this.onTap}) : super(key: key);
+  ActionButton({Key? key, required this.text, required this.onTap})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+        decoration: BoxDecoration(
+          color: SuperHeroesColors.blue,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+
         width: 150,
-        height: 30,
-        color: SuperHeroesColors.circColor,
-        child: Center(
-          child: Text(
-            '${text}',
-            style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 16,
-                color: Colors.white),
-          ),
+        height: 40,
+
+        child: Text(
+          text.toString(),
+          style: TextStyle(
+              fontWeight: FontWeight.w700, fontSize: 14,
+              color: Colors.white),
         ),
       ),
     );
